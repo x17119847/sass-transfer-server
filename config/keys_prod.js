6c573a8dafd3d4f8071124ae15b114c8117142e2
rayphelan <rayphelan@gmail.com>
2018-12-02 22:41:06 +0000

    Fixed prod keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,8 +2,8 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  sassTransferServiceAPIUser: SASS_TRANSFER_SERVICE_API_USER,
-  sassTransferServiceAPIEmail: SASS_TRANSFER_SERVICE_API_EMAIL,
-  sassTransferServiceAPIPassword: SASS_TRANSFER_SERVICE_API_PASSWORD,
-  sassTransferServiceAPIURI: SASS_TRANSFER_SERVICE_API_URL
+  sassTransferServiceAPIUser: process.env.SASS_TRANSFER_SERVICE_API_USER,
+  sassTransferServiceAPIEmail: process.env.SASS_TRANSFER_SERVICE_API_EMAIL,
+  sassTransferServiceAPIPassword: process.env.SASS_TRANSFER_SERVICE_API_PASSWORD,
+  sassTransferServiceAPIURI: process.env.SASS_TRANSFER_SERVICE_API_URL
 }
